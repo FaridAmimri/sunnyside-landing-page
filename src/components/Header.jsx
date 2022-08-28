277e602d0be9e3afdf6df4451f3bf43458e4f792
Farid Amimri <farid.amimri@gmail.com>
2022-08-28 18:46:35 +0200

    Add arrow icon + css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import styled from 'styled-components'
 import HeaderBackground from '../assets/desktop/image-header.jpg'
 import Logo from '../assets/logo.svg'
+import Arrow from '../assets/icon-arrow-down.svg'
 
 function Header() {
   return (
@@ -21,6 +22,7 @@ function Header() {
       </div>
       <div className='header-content'>
         <p>We are creatives</p>
+        <img src={Arrow} alt='Arrow-down'></img>
       </div>
     </Container>
   )
@@ -70,17 +72,29 @@ const Container = styled.header`
         font-size: 15px;
         text-transform: uppercase;
         padding: 15px 30px;
+        :hover {
+          color: #ffffff;
+          background: rgba(255, 255, 255, 0.3);
+          cursor: pointer;
+        }
       }
     }
   }
-  .header-content p {
-    font-family: 'Fraunces 9pt Soft';
-    font-style: normal;
-    font-weight: 900;
-    font-size: 56px;
-    text-align: center;
-    text-transform: uppercase;
-    letter-spacing: 8.75px;
-    padding-top: 50px;
+  .header-content {
+    p {
+      font-family: 'Fraunces 9pt Soft';
+      font-style: normal;
+      font-weight: 900;
+      font-size: 56px;
+      text-align: center;
+      text-transform: uppercase;
+      letter-spacing: 8.75px;
+      padding-top: 30px;
+    }
+    img {
+      position: absolute;
+      left: 50%;
+      top: 360px;
+    }
   }
 `
