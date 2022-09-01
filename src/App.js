e7fe2f1faa62a442ce879d5b46e9b441266644d7
Farid Amimri <farid.amimri@gmail.com>
2022-09-01 14:33:00 +0100

    Add Gallery component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Gallery />
     </div>
   );
 }
